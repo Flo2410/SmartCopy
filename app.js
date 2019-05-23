c35d26c6774e6d5356fea66ad2b7fea8efb593af
Florian Hye <florian.hye@gmail.com>
2019-05-23 22:48:56 +0200

    + added v1.0.0

---
+++ app.js
@@ -0,0 +1,192 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const dialog = electron.dialog;
+
+const fs = require('fs-extra')
+const path = require('path')
+
+let files_src = [];
+let files_search = [];
+let maching_files = [];
+let nonMachingFiles_src = [];
+let nonMachingFiles_search = [];
+
+let path_src, path_search, path_target;
+
+
+// Behalten Sie eine globale Referenz auf das Fensterobjekt.
+// Wenn Sie dies nicht tun, wird das Fenster automatisch geschlossen,
+// sobald das Objekt dem JavaScript-Garbagekollektor übergeben wird.
+
+let win;
+
+function createWindow () {
+  // Erstellen des Browser-Fensters.
+  win = new BrowserWindow({
+    titleBarStyle: "default",
+    useContentSize: true,
+    width: 385,
+    height: 380,
+    minWidth: 385,
+    minHeight: 380,
+    fullscreenable: false,
+    resizable: true,
+    maximizable: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // and load the index.html of the app.
+  win.loadFile('index.html');
+
+  win.setResizable(false);
+  win.setMenu(null);
+
+  // Öffnen der DevTools.
+  // win.webContents.openDevTools();
+
+  // Ausgegeben, wenn das Fenster geschlossen wird.
+  win.on('closed', () => {
+    // Dereferenzieren des Fensterobjekts, normalerweise würden Sie Fenster
+    // in einem Array speichern, falls Ihre App mehrere Fenster unterstützt.
+    // Das ist der Zeitpunkt, an dem Sie das zugehörige Element löschen sollten.
+    win = null;
+  });
+};
+
+// Diese Methode wird aufgerufen, wenn Electron mit der
+// Initialisierung fertig ist und Browserfenster erschaffen kann.
+// Einige APIs können nur nach dem Auftreten dieses Events genutzt werden.
+app.on('ready', createWindow);
+
+// Verlassen, wenn alle Fenster geschlossen sind.
+app.on('window-all-closed', () => {
+  // Unter macOS ist es üblich, für Apps und ihre Menu Bar
+  // aktiv zu bleiben, bis der Nutzer explizit mit Cmd + Q die App beendet.
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // Unter macOS ist es üblich ein neues Fenster der App zu erstellen, wenn
+  // das Dock Icon angeklickt wird und keine anderen Fenster offen sind.
+  if (win === null) {
+    createWindow();
+  }
+});
+
+// app.on("browser-window-created",(e, window) => {
+//   window.setMenu(null);
+// });
+
+// In dieser Datei können Sie den Rest des App-spezifischen
+// Hauptprozess-Codes einbinden. Sie können den Code auch
+// auf mehrere Dateien aufteilen und diese hier einbinden.
+
+
+
+exports.selectDirectory = (btn_id) => {
+  return new Promise((resolve, reject) => {
+    dialog.showOpenDialog(win, {
+      properties: ['openDirectory']
+    }, (filenames) => {
+      if(filenames) {
+        switch (btn_id) {
+          case "path_src":
+            path_src = filenames[0];
+            break;
+          case "path_search":
+            path_search = filenames[0];
+            break;
+          case "path_target":
+            path_target = filenames[0];
+            break;
+        }
+        resolve(filenames[0]);
+      } else {
+        console.log("nope.");
+        reject();
+      }
+    });
+  });
+}
+
+exports.check = (maching, copy) => {
+  files_src = [];
+  files_search = [];
+  maching_files = [];
+  nonMachingFiles_src = [];
+  nonMachingFiles_search = [];
+
+  fs.readdirSync(path_src).forEach((file) => {
+      files_src.push(file);
+  });
+
+  fs.readdirSync(path_search).forEach((file) => {
+    files_search.push(file);
+  });
+
+  files_search.forEach((file) => {
+    if(files_src.includes(file)) maching_files.push(file);
+    else nonMachingFiles_search.push(file);
+  });
+
+  files_src.forEach((file) => {
+    if(!files_search.includes(file)) nonMachingFiles_src.push(file);
+  });
+
+  fs.ensureDir(path_target).then(() => {
+    if(maching && copy) { // copy maching files from source
+      maching_files.forEach((file) => {
+        fs.copy(path_src + "/" + file, path_target + "/" + file).catch((err) => {
+          throw err;
+        });
+        console.log("copied: " + path_src + "/" + file + " to " + path_target + "/" + file);
+
+      });
+    } else if (!maching && copy) { // copy non maching files from source
+      nonMachingFiles_src.forEach((file) => {
+        fs.copy(path_src + "/" + file, path_target + "/" + file).catch((err) => {
+          throw err;
+        });
+        console.log("copied: " + path_src + "/" + file + " to " + path_target + "/" + file);
+
+      });
+    } else if (maching && !copy) { // copy maching files from search
+      maching_files.forEach((file) => {
+        fs.copy(path_search + "/" + file, path_target + "/" + file).catch((err) => {
+          throw err;
+        });
+        console.log("copied: " + path_search + "/" + file + " to " + path_target + "/" + file);
+
+      });
+
+    } else if (!maching && !copy) { // copy non maching files from search
+      nonMachingFiles_search.forEach((file) => {
+        fs.copy(path_search + "/" + file, path_target + "/" + file).catch((err) => {
+          throw err;
+        });
+        console.log("copied: " + path_search + "/" + file + " to " + path_target + "/" + file);
+
+      });
+    }
+
+  }).catch((err) => {
+    throw err;
+  });
+
+  console.log("path_src:", path_src);
+  console.log("path_search:", path_search);
+  console.log("path_target:", path_target);
+  console.log("maching:", maching);
+  console.log("copy:", copy);
+
+  console.log("files_src:", files_src);
+  console.log("files_search:", files_search);
+  console.log("maching_files:", maching_files);
+  console.log("nonMachingFiles_src:", nonMachingFiles_src);
+  console.log("nonMachingFiles_search:", nonMachingFiles_search);
+}
---
+++ index.js
@@ -0,0 +1,35 @@
+const electron = require('electron');
+const remote = electron.remote;
+const mainProcess = remote.require('./app');
+
+
+$("#path_src").click(() => {
+  mainProcess.selectDirectory("path_src").then((filename) => {
+    $("#path_src_file").val(filename);
+  }).catch(() => {
+    $("#path_src_file").val("Choose file");
+  });
+});
+
+
+$("#path_search").click(() => {
+  mainProcess.selectDirectory("path_search").then((filename) => {
+    $("#path_search_file").val(filename);
+  }).catch(() => {
+    $("#path_search_file").val("Choose file");
+  });
+});
+
+$("#path_target").click(() => {
+  mainProcess.selectDirectory("path_target").then((filename) => {
+    $("#path_target_file").val(filename);
+  }).catch(() => {
+    $("#path_target_file").val("Choose file");
+  });
+});
+
+$("#check").click(() => {
+  const maching = $("#select_maching").val();
+  const copy = $("#select_copy").val();
+  mainProcess.check(JSON.parse(maching), JSON.parse(copy));
+});
